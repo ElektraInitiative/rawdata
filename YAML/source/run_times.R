bc42ece9763f35cc58f7c85b318533dce6d3db9c
René Schwaiger <sanssecours@me.com>
2019-05-14 15:06:54 +0200

    YAML Benchmark: Add data analysis script

---
+++ YAML/source/run_times.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+library(jsonlite)
+library(tidyverse)
+
+data <- read_json("YAML/results/generated.json", simplifyVector = TRUE)
+data <- data$results
+
+plugins <- data$command %>%
+  map_chr(~ sub(".* ([a-z]+) get$", "\\1", .))
+times <-data$times
+
+plugin_times <- tibble(plugin = character(), runtime = double())
+for (index in c(1:length(plugins))) {
+  plugin_times <- add_row(plugin_times, plugin = plugins[[index]],
+                          runtime = times[[index]])
+}
