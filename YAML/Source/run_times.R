2cb4db09b19c474eca6fe9cde039a39ba96952bc
René Schwaiger <sanssecours@me.com>
2019-05-16 16:22:35 +0200

    YAML Benchmark: Reformat code

--- YAML/Source/run_times.R
+++ YAML/Source/run_times.R
@@ -12,34 +12,35 @@ library(tidyverse)
 # ========
 
 result_directory = "YAML/Results"
-plugin_times <- tibble(plugin = character(), 
-                       runtime = double(), 
-                       lines = numeric(), 
-                       os = character(), 
+plugin_times <- tibble(plugin = character(),
+                       runtime = double(),
+                       lines = numeric(),
+                       os = character(),
                        compiler = character())
 
 files <- list.files(result_directory, pattern="generated.*\\.json")
 for (filepath in files) {
   fields <- strsplit(filepath, "_")
   fields <- fields[[1]]
-  
+
   lines <- fields[2]
   os <- fields[3]
   compiler <- fields[4]
   compiler <- sub("(.*)\\.json$", "\\1", compiler)
-  
-  data <- read_json(paste(result_directory, filepath, sep="/"), simplifyVector = TRUE)
+
+  data <- read_json(paste(result_directory, filepath, sep="/"),
+                    simplifyVector = TRUE)
   data <- data$results
-  
+
   plugins <- data$command %>%
     map_chr(~ sub(".* ([a-z]+) get$", "\\1", .))
   times <-data$times
-  
+
   for (index in c(1:length(plugins))) {
-    plugin_times <- add_row(plugin_times, 
+    plugin_times <- add_row(plugin_times,
                             plugin = plugins[[index]],
-                            runtime = times[[index]], 
-                            lines = lines, 
+                            runtime = times[[index]],
+                            lines = lines,
                             os = os,
                             compiler = compiler)
   }
@@ -50,5 +51,5 @@ for (filepath in files) {
 # =============
 
 ggplot(data = plugin_times) +
-  geom_point(mapping = aes(x = lines, y = runtime, color = plugin)) + 
+  geom_point(mapping = aes(x = lines, y = runtime, color = plugin)) +
   facet_wrap(~ os, nrow = 2)
